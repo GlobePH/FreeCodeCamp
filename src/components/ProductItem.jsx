f2bebf424422939d095b69cccb6a33adc9bf11f3
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 20:57:32 +0800

    fix tablet product grid

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class ProductItem extends Component {
   render() {
     return (
-      <div className="product-item col-md-4 text-center">
+      <div className="product-item col-md-4 col-sm-6 text-center">
         <a className="product-content" href="/crops/test">
           <div className="thumbnail product-thumbnail"
             style={{backgroundImage: `url(${this.props.imgURL})`}}
