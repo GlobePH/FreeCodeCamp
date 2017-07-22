06a697d3ecce3dccf1e4409ae22f204f8f95c17e
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-23 07:17:29 +0800

    Rename catalogue to crops

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,57 +2,63 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Logout from "./Logout";
 import { Link } from "react-router-dom";
-import Media from 'react-media'
+import Media from "react-media";
 
 import "../styles/hamburger.css";
 import "../styles/header.css";
 
-
 class Header extends Component {
-
-  render(){
+  render() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
           <div className="navbar-header pull-left">
             <div className="navbar-brand">
-              <Link to="/"><i className="fa fa-free-code-camp" aria-hidden="true"></i>. Farmbase</Link>
+              <Link to="/">
+                <i className="fa fa-free-code-camp" aria-hidden="true" />.
+                Farmbase
+              </Link>
             </div>
             <Logout />
           </div>
 
           <Media query="(min-width: 768px)">
-            {matches => matches ? (
-              <div className="pull-right">
-                <div className="account-nav pull-right">
-                  <Link to="/cart">
-                    <i className="fa fa-shopping-cart" aria-hidden="true" />Cart
-                  </Link>
-                </div>
-                <ul className="nav navbar-nav pull-right">
-                  <li className="active">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/crops">Catalogue</Link>
-                  </li>
-                  <li>
-                    <a href="/">Page 2</a>
-                  </li>
-                  <li>
-                    <a href="/">Page 3</a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <div className="pull-right">
-                <button className="hamburger hamburger--elastic" type="button">
-                  <span className="hamburger-box">
-                    <span className="hamburger-inner"></span>
-                  </span>
-                </button>
-              </div>
-            )}
+            {matches =>
+              matches
+                ? <div className="pull-right">
+                    <div className="account-nav pull-right">
+                      <Link to="/cart">
+                        <i
+                          className="fa fa-shopping-cart"
+                          aria-hidden="true"
+                        />Cart
+                      </Link>
+                    </div>
+                    <ul className="nav navbar-nav pull-right">
+                      <li className="active">
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/crops">Crops</Link>
+                      </li>
+                      <li>
+                        <a href="/">Page 2</a>
+                      </li>
+                      <li>
+                        <a href="/">Page 3</a>
+                      </li>
+                    </ul>
+                  </div>
+                : <div className="pull-right">
+                    <button
+                      className="hamburger hamburger--elastic"
+                      type="button"
+                    >
+                      <span className="hamburger-box">
+                        <span className="hamburger-inner" />
+                      </span>
+                    </button>
+                  </div>}
           </Media>
         </div>
       </nav>
