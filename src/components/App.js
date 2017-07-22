dc827b24ea6ef410879e59864253c9173591d7c1
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-22 09:54:48 +0800

    Add firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import firebase from "firebase";
-import firebaseui from "firebaseui";
-
-import Header from './Header';
+import * as firebase from "firebase";
+import "firebase/database";
 
 firebase.initializeApp({
   apiKey: "AIzaSyAQPIoN8G0GV9hYd1mN30Y6mKjFLGQhnqA",
@@ -13,11 +11,23 @@ firebase.initializeApp({
   messagingSenderId: "246767341490"
 });
 
+const db = firebase.database();
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { firebaseTester: "asdfjanskjdfhaskjdf" };
+    db.ref().on("value", snap => {
+      this.setState({ firebaseTester: snap.val().testKey });
+    });
+  }
+
   render() {
     return (
       <div>
-        <Header/>
+        <span style={{ position: "absolute", left: "50%", top: "50%" }}>
+          {this.state.firebaseTester}
+        </span>
       </div>
     );
   }
