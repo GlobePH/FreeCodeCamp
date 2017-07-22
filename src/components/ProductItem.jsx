e3b254f2f3b62e9cf4ef15476f824314e43c0236
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 16:17:45 +0800

    Added Style fixes to Product Index

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,13 +4,13 @@ export default class ProductItem extends Component {
   render() {
     return(
       <div className="product-item col-md-4 text-center">
-        <div className="product-content">
+        <a className="product-content" href="#">
           <div className="thumbnail product-thumbnail">
             <img src="http://via.placeholder.com/350x350" alt="product" />
           </div>
-          <h3 className="product-title"><a href="#">Product Name</a></h3>
+          <h3 className="product-title">Product Name</h3>
           <p className="product-price">$1.00</p>
-        </div>
+        </a>
       </div>
     );
   }
