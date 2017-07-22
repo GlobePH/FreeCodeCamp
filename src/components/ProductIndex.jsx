303031c749ebc482394a6d4e702baf4745139644
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 15:44:46 +0800

    fix Product Index Component

--- src/components/ProductIndex.jsx
+++ src/components/ProductIndex.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ProductItem from './ProductItem';
+import "../styles/productIndex.css";
 
 export default class ProductIndex extends Component {
   constructor() {
@@ -9,6 +11,17 @@ export default class ProductIndex extends Component {
     return(
       <div className="ProductIndex">
         <p>test Product Index</p>
+      <div className="product-index">
+        <div className="container product-list">
+          <div className="row">
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+          </div>
+        </div>
       </div>
     );
   }
