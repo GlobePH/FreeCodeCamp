eb9258c36393675219aa40fb3808b826744cb252
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 17:47:10 +0800

    Add layouting to Product detail

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -5,26 +5,33 @@ export default class ProductDetail extends Component {
     return(
       <div className="product-detail">
         <div className="container">
-          <div className="row product-detail-top">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="row">
+                <div className="col-md-5">
+                  <div className="product-detail-image">
+                    <img src="http://via.placeholder.com/350x350" alt="" className="img-responsive"/>
+                  </div>
+                </div>
 
-            <div className="col-md-6">
-              <div className="product-detail-image">
-                <img src="http://via.placeholder.com/350x350" alt="" className="img-responsive"/>
+                <div className="col-md-7">
+                  <div className="product-detail-summary">
+                    <h1 className="pd-title">Product Name</h1>
+                    <p className="pd-price">$1.00</p>
+                    <form className="form-inline">
+                      <div className="form-group">
+                        <label>Quantity:</label>
+                        <input type="number" className="form-control" />
+                      </div>
+                    </form>
+                    <a href="#" className="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div className="col-md-6">
-              <div className="product-detail-summary">
-                <h1 className="pd-title">Product Name</h1>
-                <p className="pd-price">$1.00</p>
-                <form className="form-inline">
-                  <div className="form-group">
-                    <label>Quantity:</label>
-                    <input type="number" className="form-control" />
-                  </div>
-                </form>
-                <a href="#" className="btn btn-primary">Add to Cart</a>
-              </div>
+            <div className="col-md-3">
+              Add cart here...
             </div>
 
           </div>
