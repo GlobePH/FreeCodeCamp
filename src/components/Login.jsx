176ca3c79a196f629b3221ab8db1482fc10eb17f
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 11:04:21 +0800

    Resolve conflict in Login COmponent

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../styles/login.css";
 
 export default class Login extends Component {
 
@@ -6,10 +7,23 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button onClick={this.handleLogin.bind(this)}>Login</button>
+      <div className="login-container container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <form>
+              <div className="app-logo text-center"><h1>Farmbase</h1></div>
+              <div className="form-group">
+                <label for="email">Email:</label>
+                <input className="form-control" type="email" placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <label for="email">Password:</label>
+                <input className="form-control" type="password" placeholder="Password" />
+              </div>
+              <button onClick={this.handleLogin.bind(this)} className="btn btn-default">Login</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
