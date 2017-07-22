c558a1fc7d233e022b5975b363c283553bed8b07
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 17:29:13 +0800

    Fix product image sizes

--- src/components/ProductIndex.jsx
+++ src/components/ProductIndex.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { fbDatabase } from "../fire";
 import ProductItem from "./ProductItem";
+import "../styles/productIndex.css";
 
 export default class ProductIndex extends Component {
   constructor() {
@@ -27,7 +28,7 @@ export default class ProductIndex extends Component {
               <ProductItem key={crop.id} name={crop.name} imgURL={crop.imgURL} />
             )}
           </div>
-        </div>    
+        </div>
       </div>
     );
   }
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,14 +5,13 @@ export default class ProductItem extends Component {
     return (
       <div className="product-item col-md-4 text-center">
         <a className="product-content" href="#">
-          <div className="thumbnail product-thumbnail">
-            <img src={this.props.imgURL} alt="product" />
+          <div className="thumbnail product-thumbnail"
+            style={{backgroundImage: `url(${this.props.imgURL})`}}
+          >
           </div>
 
           <h3 className="product-title">
-            <a href="eslint">
               {this.props.name}
-            </a>
           </h3>
           <p className="product-price">$1.00</p>
         </a>
