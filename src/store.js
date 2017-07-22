f93dbc375bc0987379de3935e612ca9967911a25
cauldyclark15 <jcing.jc@gmail.com>
2017-07-22 15:09:59 +0800

    Fix reducer

--- src/store.js
+++ src/store.js
@@ -1,14 +1,10 @@
 import { createStore, compose } from 'redux'
 import reducer from './reducer'
 
-<<<<<<< HEAD
 const composeEnhancers = typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose 
 const store = createStore(
   reducer,
   composeEnhancers()
 )
-=======
-const store = createStore(reducer);
->>>>>>> Update
 
 export default store
