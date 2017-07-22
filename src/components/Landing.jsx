69dd390eec6e420935bda30c76c399a00b1b7db0
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-23 00:24:06 +0800

    styling banner

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,9 +7,17 @@ const Landing = () => {
       <div className="home-banner container-fluid">
         <div className="home-banner-content text-center">
           <h1>Tag Line Header here please.</h1>
-          <Link className="btn btn-primary shop-link" to="/crops">Shop Now</Link>
+          <div className="text-content">
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus</p>
+          </div>
+          
+          <Link className="btn btn-primary shop-link" to="/crops">Shop Now 
+            <span className="fa fa-chevron-right"></span>
+          </Link>
         </div>  
       </div>
+
+      <div style={{minHeight: '700px'}}/>
     </div>
   );
 };
