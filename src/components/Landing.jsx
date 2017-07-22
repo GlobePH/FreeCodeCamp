a5ce9556a8fc8a34fe9f6e1c3e2cd21d63ad3e6b
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-22 22:20:11 +0800

    reset landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,18 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
-<<<<<<< HEAD
   return <div />;
-=======
-  return (
-    <div>
-      <Link to="/login">
-        <button>Go to login</button>
-      </Link>
-      <button onClick={event => alert('test')}>Create</button>
-    </div>
-  );
->>>>>>> install react pdf
 };
 
 
