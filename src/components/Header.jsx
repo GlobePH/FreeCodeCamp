89322cfa249e9fbbb69177f31ec7275c640781d9
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-07-23 07:37:15 +0800

    fix conflict again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,20 @@ import "../styles/hamburger.css";
 import "../styles/header.css";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mmenuOpen: false
+    };
+  }
+
   handleHamburgerClick() {
     this.setState({ mmenuOpen: !this.state.mmenuOpen });
     console.log(this.state.mmenuOpen);
   }
 
   render() {
-    // const { mmenuOpen } = this.state;
+    const { mmenuOpen } = this.state;
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
@@ -29,42 +36,55 @@ class Header extends Component {
           </div>
 
           <Media query="(min-width: 768px)">
-            {matches =>
-              matches
-                ? <div className="pull-right">
-                    <div className="account-nav pull-right">
-                      <Link to="/cart">
-                        <i
-                          className="fa fa-shopping-cart"
-                          aria-hidden="true"
-                        />Cart
-                      </Link>
-                    </div>
-                    <ul className="nav navbar-nav pull-right">
-                      <li className="active">
-                        <Link to="/">Home</Link>
-                      </li>
-                      <li>
-                        <Link to="/crops">Crops</Link>
-                      </li>
-                      <li>
-                        <a href="/">Page 2</a>
-                      </li>
-                      <li>
-                        <a href="/">Page 3</a>
-                      </li>
-                    </ul>
-                  </div>
-                : <div className="pull-right">
-                    <button
-                      className="hamburger hamburger--elastic"
-                      type="button"
-                    >
-                      <span className="hamburger-box">
-                        <span className="hamburger-inner" />
-                      </span>
-                    </button>
-                  </div>}
+            {matches => matches ? (
+              <div className="pull-right">
+                <div className="account-nav pull-right">
+                  <Link to="/cart">
+                    <i className="fa fa-shopping-cart" aria-hidden="true" />Cart
+                  </Link>
+                </div>
+                <ul className="nav navbar-nav pull-right">
+                  <li className="active">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/crops">Catalogue</Link>
+                  </li>
+                  <li>
+                    <a href="/">Page 2</a>
+                  </li>
+                  <li>
+                    <a href="/">Page 3</a>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div className="pull-right">
+                <button className={!mmenuOpen ? 'hamburger hamburger--elastic' : 'hamburger hamburger--elastic is-active'} type="button" onClick={this.handleHamburgerClick.bind(this)}>
+                  <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                  </span>
+                </button>
+                <div className={mmenuOpen ? 'mmenu-overlay open' : 'mmenu-overlay'} onClick={this.handleHamburgerClick.bind(this)}></div>
+                <div className={mmenuOpen ? 'mmenu open' : 'mmenu'}>
+                  <p className="mmnenu-close" onClick={this.handleHamburgerClick.bind(this)}>&times;</p>
+                  <ul className="mmenu-list">
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/crops">Catalogue</Link>
+                    </li>
+                    <li>
+                      <a href="/">Page 2</a>
+                    </li>
+                    <li>
+                      <a href="/">Page 3</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            )}
           </Media>
         </div>
       </nav>
