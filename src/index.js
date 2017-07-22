83c912c73217cf963c2ac0e092bcff98163860de
cauldyclark15 <jcing.jc@gmail.com>
2017-07-22 11:22:14 +0800

    Remove serviceworkers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from 'react-router-dom';
-import registerServiceWorker from "./utils/registerServiceWorker";
 
 import "./styles/index.css";
 
@@ -14,4 +13,3 @@ ReactDOM.render(
   </BrowserRouter>, 
   document.getElementById("root")
 );
-registerServiceWorker();
