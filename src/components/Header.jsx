bae1cb60f67a59a4483cc1eb1dc589470fe364a8
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-23 07:34:27 +0800

    Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,13 @@ import "../styles/hamburger.css";
 import "../styles/header.css";
 
 class Header extends Component {
-<<<<<<< HEAD
-  render() {
-=======
-  constructor(props) {
-    super(props);
-    this.state = {
-      mmenuOpen: false
-    };
-  }
-
   handleHamburgerClick() {
     this.setState({ mmenuOpen: !this.state.mmenuOpen });
     console.log(this.state.mmenuOpen);
   }
 
-  render(){
-    const {mmenuOpen} = this.state
->>>>>>> c0cee7896d8fb5beb955462da6b4ed0a49685a84
+  render() {
+    // const { mmenuOpen } = this.state;
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
@@ -40,7 +29,6 @@ class Header extends Component {
           </div>
 
           <Media query="(min-width: 768px)">
-<<<<<<< HEAD
             {matches =>
               matches
                 ? <div className="pull-right">
@@ -77,64 +65,12 @@ class Header extends Component {
                       </span>
                     </button>
                   </div>}
-=======
-            {matches => matches ? (
-              <div className="pull-right">
-                <div className="account-nav pull-right">
-                  <Link to="/cart">
-                    <i className="fa fa-shopping-cart" aria-hidden="true" />Cart
-                  </Link>
-                </div>
-                <ul className="nav navbar-nav pull-right">
-                  <li className="active">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/crops">Catalogue</Link>
-                  </li>
-                  <li>
-                    <a href="/">Page 2</a>
-                  </li>
-                  <li>
-                    <a href="/">Page 3</a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <div className="pull-right">
-                <button className={!mmenuOpen ? 'hamburger hamburger--elastic' : 'hamburger hamburger--elastic is-active'} type="button" onClick={this.handleHamburgerClick.bind(this)}>
-                  <span className="hamburger-box">
-                    <span className="hamburger-inner"></span>
-                  </span>
-                </button>
-                <div className={mmenuOpen ? 'mmenu-overlay open' : 'mmenu-overlay'} onClick={this.handleHamburgerClick.bind(this)}></div>
-                <div className={mmenuOpen ? 'mmenu open' : 'mmenu'}>
-                  <p className="mmnenu-close" onClick={this.handleHamburgerClick.bind(this)}>&times;</p>
-                  <ul className="mmenu-list">
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                      <Link to="/crops">Catalogue</Link>
-                    </li>
-                    <li>
-                      <a href="/">Page 2</a>
-                    </li>
-                    <li>
-                      <a href="/">Page 3</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            )}
->>>>>>> c0cee7896d8fb5beb955462da6b4ed0a49685a84
           </Media>
         </div>
       </nav>
     );
   }
 }
-// };
 
 function mapStateToProps(state) {
   return {
