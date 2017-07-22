f0c556c3ca7eb3135d398945d25c3ad0da306580
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-22 14:01:03 +0800

    Add Firebase config

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <div>
-      <h1>
-        I am Farmbase
-      </h1>
-      <Link to='/login'>
+      <Link to="/login">
         <button>Go to login</button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
--- src/fire.js
+++ src/fire.js
@@ -1,4 +1,15 @@
-import firebase from 'firebase'
-import 'firebase/database'
+import * as firebase from "firebase";
+import "firebase/database";
+import "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAQPIoN8G0GV9hYd1mN30Y6mKjFLGQhnqA",
+  authDomain: "globe-hackathon.firebaseapp.com",
+  databaseURL: "https://globe-hackathon.firebaseio.com",
+  projectId: "globe-hackathon",
+  storageBucket: "globe-hackathon.appspot.com",
+  messagingSenderId: "246767341490"
+};
 
+export const fbDatabase = firebase.database();
+export const fbAuth = firebase.auth();
