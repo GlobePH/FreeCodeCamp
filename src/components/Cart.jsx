9781e943cd5b0c99b08f04c9a563bbee5f0626d3
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-23 09:09:53 +0800

    Fix min-vh tracking

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,37 +1,42 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Cart extends Component {
-  render () {
-    const {cart} = this.props
-    let cropsItem
+  render() {
+    const { cart } = this.props;
+    let cropsItem;
 
     if (cart) {
-      cropsItem = cart.map(item => (
+      cropsItem = cart.map(item =>
         <div>
-          <h4>Crop Name: <b>{item.name}</b></h4>
-          <p>Quantity: {item.quantity}</p>
-          <p>Price: ₱{Math.floor(Math.random() * 10 * item.quantity).toFixed(2)}</p>
+          <h4>
+            Crop Name: <b>{item.name}</b>
+          </h4>
+          <p>
+            Quantity: {item.quantity}
+          </p>
+          <p>
+            Price: ₱{Math.floor(Math.random() * 10 * item.quantity).toFixed(2)}
+          </p>
         </div>
-      ))
+      );
     } else {
-      cropsItem = <p>Add a Crop to Cart</p>
+      cropsItem = <p>Add a Crop to Cart</p>;
     }
 
     return (
-      <div>
+      <div style={{ height: "89vh" }}>
         <h3>Cart Items</h3>
         {cropsItem}
         <Link to="/tracking-purchase">Continue to Checkout</Link>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  cart: state.cart,
-})
+const mapStateToProps = state => ({
+  cart: state.cart
+});
 
-export default connect(mapStateToProps)(Cart)
-
+export default connect(mapStateToProps)(Cart);
