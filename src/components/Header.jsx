47a8e9cdf7c8f839d4c7379dfddf91d32dc6a1e1
John Salcedo <johnsalcedonetwork@gmail.com>
2017-07-23 09:00:51 +0800

    Add login link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,55 +35,72 @@ class Header extends Component {
           </div>
 
           <Media query="(min-width: 768px)">
-            {matches => matches ? (
-              <div className="pull-right">
-                <div className="account-nav pull-right">
-                  <Link to="/cart">
-                    <i className="fa fa-shopping-cart" aria-hidden="true" />Cart
-                  </Link>
-                </div>
-                <ul className="nav navbar-nav pull-right">
-                  <li className="active">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/crops">Catalogue</Link>
-                  </li>
-                  <li>
-                    <a href="/">Page 2</a>
-                  </li>
-                  <li>
-                    <a href="/">Page 3</a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <div className="pull-right">
-                <button className={!mmenuOpen ? 'hamburger hamburger--elastic' : 'hamburger hamburger--elastic is-active'} type="button" onClick={this.handleHamburgerClick.bind(this)}>
-                  <span className="hamburger-box">
-                    <span className="hamburger-inner"></span>
-                  </span>
-                </button>
-                <div className={mmenuOpen ? 'mmenu-overlay open' : 'mmenu-overlay'} onClick={this.handleHamburgerClick.bind(this)}></div>
-                <div className={mmenuOpen ? 'mmenu open' : 'mmenu'}>
-                  <p className="mmnenu-close" onClick={this.handleHamburgerClick.bind(this)}>&times;</p>
-                  <ul className="mmenu-list">
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                      <Link to="/crops">Catalogue</Link>
-                    </li>
-                    <li>
-                      <a href="/">Page 2</a>
-                    </li>
-                    <li>
-                      <a href="/">Page 3</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            )}
+            {matches =>
+              matches
+                ? <div className="pull-right">
+                    <div className="account-nav pull-right">
+                      <Link to="/cart">
+                        <i
+                          className="fa fa-shopping-cart"
+                          aria-hidden="true"
+                        />Cart
+                      </Link>
+                    </div>
+                    <ul className="nav navbar-nav pull-right">
+                      <li className="active">
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/crops">Crops</Link>
+                      </li>
+                      <li>
+                        <a href="/login">Login</a>
+                      </li>
+                    </ul>
+                  </div>
+                : <div className="pull-right">
+                    <button
+                      className={
+                        !mmenuOpen
+                          ? "hamburger hamburger--elastic"
+                          : "hamburger hamburger--elastic is-active"
+                      }
+                      type="button"
+                      onClick={this.handleHamburgerClick.bind(this)}
+                    >
+                      <span className="hamburger-box">
+                        <span className="hamburger-inner" />
+                      </span>
+                    </button>
+                    <div
+                      className={
+                        mmenuOpen ? "mmenu-overlay open" : "mmenu-overlay"
+                      }
+                      onClick={this.handleHamburgerClick.bind(this)}
+                    />
+                    <div className={mmenuOpen ? "mmenu open" : "mmenu"}>
+                      <p
+                        className="mmnenu-close"
+                        onClick={this.handleHamburgerClick.bind(this)}
+                      >
+                        &times;
+                      </p>
+                      <ul className="mmenu-list">
+                        <li>
+                          <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                          <Link to="/crops">Catalogue</Link>
+                        </li>
+                        <li>
+                          <a href="/">Page 2</a>
+                        </li>
+                        <li>
+                          <a href="/">Page 3</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>}
           </Media>
         </div>
       </nav>
